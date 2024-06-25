5884144362ce0f7f8a8a4de8de8450b16282814c
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-06-24 21:24:07 -0400

    maocs support

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,56 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/EffectGameObject.hpp>
 #include <map>
+#ifdef __APPLE__
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+std::map<int, std::string> cameraIDToSetting = {
+	{ 1913, "disableZoom" },
+	{ 1914, "disableStatic" },
+	{ 1916, "disableOffset" },
+	{ 2015, "disableRotate" },
+	{ 2016, "disableGuide" },
+	{ 2062, "disableEdge" },
+	{ 2901, "disableGPOffset" },
+	{ 2925, "disableMode" }
+};
+
+std::map<int, std::string> shaderIDToSetting = {
+	{ 2904, "disableShader" },
+	{ 2905, "disableShockWave" },
+	{ 2907, "disableShockLine" },
+	{ 2909, "disableGlitch" },
+	{ 2910, "disableChromatic" },
+	{ 2911, "disableChromaticGlitch" },
+	{ 2912, "disablePixelate" },
+	{ 2913, "disableLensCircle" },
+	{ 2914, "disableRadialBlur" },
+	{ 2915, "disableMotionBlur" },
+	{ 2916, "disableBulge" },
+	{ 2917, "disablePinch" },
+	{ 2919, "disableGrayscale" },
+	{ 2920, "disableSepia" },
+	{ 2921, "disableInvertColor" },
+	{ 2922, "disableHue" },
+	{ 2923, "disableEditColor" },
+	{ 2924, "disableSplitScreen" }
+};
+
+class $modify(MyPlayLayer, PlayLayer) {
+	void addObject(GameObject* p0) {
+	        if (Mod::get()->getSettingValue<bool>("enabled")) {
+			bool dontSkip = true;
+			auto id = p0->m_objectID;
+			if (cameraIDToSetting.find(id) != cameraIDToSetting.end() && Mod::get()->getSettingValue<bool>(cameraIDToSetting.find(id)->second)) { dontSkip = false; }
+			if (shaderIDToSetting.find(id) != shaderIDToSetting.end() && Mod::get()->getSettingValue<bool>(shaderIDToSetting.find(id)->second)) { dontSkip = false; }
+			if (dontSkip) { PlayLayer::addObject(p0); }
+		} else {
+			PlayLayer::addObject(p0);
+		}
+	}
+};
+#else
+#include <Geode/modify/EffectGameObject.hpp>
 
 using namespace geode::prelude;
 
@@ -40,10 +89,11 @@ std::map<std::string, std::string> shaderFrameToSetting = {
 class $modify(MyEffectGameObject, EffectGameObject) {
 	bool init(char const* p0) {
 		if (!EffectGameObject::init(p0)) { return false; }
-		if (GameManager::sharedState()->getPlayLayer() == nullptr) { return true; }
+		if (PlayLayer::get() == nullptr) { return true; }
 		if (!Mod::get()->getSettingValue<bool>("enabled")) { return true; }
 		if (cameraFrameToSetting.find(p0) != cameraFrameToSetting.end() && Mod::get()->getSettingValue<bool>(cameraFrameToSetting.find(p0)->second)) { return false; }
 		if (shaderFrameToSetting.find(p0) != shaderFrameToSetting.end() && Mod::get()->getSettingValue<bool>(shaderFrameToSetting.find(p0)->second)) { return false; }
 		return true;
 	}
 };
+#endif
